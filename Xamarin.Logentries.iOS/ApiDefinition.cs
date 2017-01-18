9339c231bfd4987edd6450f5bc7a9e208d6b0c46
Alex Sorokoletov <alex.sorokoletov@gmail.com>
2017-01-18 17:57:00 -0500

    ios binary updated, nuspec added

--- Xamarin.Logentries.iOS/ApiDefinition.cs
+++ Xamarin.Logentries.iOS/ApiDefinition.cs
@@ -29,7 +29,19 @@ namespace Logentries.iOS
         [Export("log:")]
         void Log(NSObject @object);
 
+        /// <summary>
+        ///  Display all messages on TTY for debug purposes
+        /// </summary>
+        /// <value><c>true</c> if debug logging to console; otherwise, <c>false</c>.</value>
+        //@property(nonatomic) BOOL debugLogs;
+        [Export("debugLogs")]
+        bool DebugLoggingToConsole { get; set; }
+
         // @property (assign, nonatomic) BOOL logApplicationLifecycleNotifications;
+        /// <summary>
+        /// Log UIApplicationDidFinishLaunchingNotification, UIApplicationDidBecomeActiveNotification, UIApplicationWillEnterForegroundNotification, UIApplicationWillResignActiveNotification, UIApplicationWillTerminateNotification.
+        /// </summary>
+        /// <value><c>true</c> if log application lifecycle notifications; otherwise, <c>false</c>.</value>
         [Export("logApplicationLifecycleNotifications")]
         bool LogApplicationLifecycleNotifications { get; set; }
     }
